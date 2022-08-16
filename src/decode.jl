bbbb17c6e8972a422e50c8617b1e621b161deb54
Ashwani Rathee <ab669522@gmail.com>
2022-08-16 16:25:29 +0530

    use of color palette, structure similiar to imagemagick(likely slow)

--- src/decode.jl
+++ src/decode.jl
@@ -6,33 +6,33 @@ using FileIO
 function gif_decode(path)
     Error = Cint(0)
     gif = LibGif.DGifOpenFileName(path, Ref(Error))
-    final = []
+
     try
         gif == C_NULL && error("failed to open the gif file: null pointer")
         slurpReturn = LibGif.DGifSlurp(gif)
+
         if (slurpReturn != LibGif.GIF_OK)
             println("Failed to read .gif file")
             return false
         end
-
-
         N = unsafe_load(gif)
         components = unsafe_wrap(Array, N.SavedImages, N.ImageCount)
-        
-        for i in 1:N.ImageCount
+        d = unsafe_load(N.SColorMap)
+        palette = unsafe_wrap(Array, d.Colors, d.ColorCount)
+        final = zeros(RGB{N0f8}, N.SHeight, N.SWidth, N.ImageCount)
+        for i = 1:N.ImageCount
             #erro handling for this
-            img = unsafe_wrap(Array, components[i].RasterBits, N.SWidth * N.SHeight) 
-            # get type of colorspace n make it all fast by doing it all in cpp
-            res = reinterpret(Gray{N0f8}, img)
-
+            img = unsafe_wrap(Array, components[i].RasterBits, N.SWidth * N.SHeight)
+            colortypes = map(x -> palette[x+1], img)
+            res = map(x -> RGB{N0f8}(x.Red / 255, x.Green / 255, x.Blue / 255), colortypes)
             res = reshape(res, Int(N.SWidth), Int(N.SHeight))
-            res =  res'
-            push!(final, res)
+            res = res'
+            final[:, :, i] = res
         end
+        return final
     finally
         # too many opened files issue
         # error handling issue in error here
-        LibGif.EGifCloseFile(gif, Ref(Error));
+        LibGif.EGifCloseFile(gif, Ref(Error))
     end
-    return final
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,23 +7,12 @@ _wrap(name) = "https://github.com/ashwani-rathee/gif-sampleimages/blob/main/$(na
 get_example(x) = Downloads.download(_wrap(x))
 
 @testset "GIFImages.jl" begin
-    @test size(gif_decode(get_example("fire.gif")))[1] == 33
-    @test size(gif_decode(get_example("gifgrid.gif")))[1] == 1
-    @test size(gif_decode(get_example("porsche.gif")))[1] == 1
-    @test size(gif_decode(get_example("solid2.gif")))[1] == 1
-    @test size(gif_decode(get_example("treescap-interlaced.gif")))[1] == 1
-    @test size(gif_decode(get_example("treescap.gif")))[1] == 1
-    @test size(gif_decode(get_example("welcome2.gif")))[1] == 6
-    @test size(gif_decode(get_example("x-trans.gif")))[1] == 1
-
-
-    @test size(gif_decode(get_example("fire.gif"))[1]) == (60, 30)
-    @test size(gif_decode(get_example("gifgrid.gif"))[1]) == (100, 100)
-    @test size(gif_decode(get_example("porsche.gif"))[1]) == (200, 320)
-    @test size(gif_decode(get_example("solid2.gif"))[1]) == (400, 640)
-    @test size(gif_decode(get_example("treescap-interlaced.gif"))[1]) == (40, 40)
-    @test size(gif_decode(get_example("treescap.gif"))[1]) ==(40, 40)
-    @test size(gif_decode(get_example("welcome2.gif"))[1]) == (48, 290)
-    @test size(gif_decode(get_example("x-trans.gif"))[1]) == (100, 100)
-
+    @test size(gif_decode(get_example("fire.gif"))) == (60,30,33)
+    @test size(gif_decode(get_example("gifgrid.gif")))== (100,100,1)
+    @test size(gif_decode(get_example("porsche.gif"))) == (200,320,1)
+    @test size(gif_decode(get_example("solid2.gif"))) == (400,640,1)
+    @test size(gif_decode(get_example("treescap-interlaced.gif"))) == (40,40,1)
+    @test size(gif_decode(get_example("treescap.gif"))) == (40,40,1)
+    @test size(gif_decode(get_example("welcome2.gif"))) == (48,290,6)
+    @test size(gif_decode(get_example("x-trans.gif"))) == (100,100,1)
 end
