81a84a73ba90c69a8424672d12a3b60f00b229a5
Ashwani Rathee <ab669522@gmail.com>
2022-08-16 11:30:28 +0530

    memory related fixes and compat entry add

--- src/decode.jl
+++ src/decode.jl
@@ -6,35 +6,33 @@ using FileIO
 function gif_decode(path)
     Error = Cint(0)
     gif = LibGif.DGifOpenFileName(path, Ref(Error))
-
-    if (gif == C_NULL) 
-        println("Failed to open .gif, return error with type \n")
-        return false;
-    end
-
-    slurpReturn = LibGif.DGifSlurp(gif)
-    if (slurpReturn != LibGif.GIF_OK)
-        println("Failed to read .gif file")
-        return false
-    end
-
-
-    N = unsafe_load(gif)
-    components = unsafe_wrap(Array, N.SavedImages, N.ImageCount)
     final = []
-    for i in 1:N.ImageCount
-        #erro handling for this
-        img = unsafe_wrap(Array, components[i].RasterBits, N.SWidth * N.SHeight) 
-        # get type of colorspace n make it all fast by doing it all in cpp
-        res = reinterpret(Gray{N0f8}, img)
+    try
+        gif == C_NULL && error("failed to open the gif file: null pointer")
+        slurpReturn = LibGif.DGifSlurp(gif)
+        if (slurpReturn != LibGif.GIF_OK)
+            println("Failed to read .gif file")
+            return false
+        end
 
-        res = reshape(res, Int64(N.SWidth), Int64(N.SHeight))
-        res =  res'
-        push!(final, res)
+
+        N = unsafe_load(gif)
+        components = unsafe_wrap(Array, N.SavedImages, N.ImageCount)
+        
+        for i in 1:N.ImageCount
+            #erro handling for this
+            img = unsafe_wrap(Array, components[i].RasterBits, N.SWidth * N.SHeight) 
+            # get type of colorspace n make it all fast by doing it all in cpp
+            res = reinterpret(Gray{N0f8}, img)
+
+            res = reshape(res, Int(N.SWidth), Int(N.SHeight))
+            res =  res'
+            push!(final, res)
+        end
+    finally
+        # too many opened files issue
+        # error handling issue in error here
+        LibGif.EGifCloseFile(gif, Ref(Error));
     end
-
-    # too many opened files issue
-    # error handling issue in error here
-    LibGif.EGifCloseFile(gif, Ref(Error));
     return final
 end
