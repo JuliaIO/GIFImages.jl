a78c9a1236127e4947ab48cbdb78857695e8603e
Ashwani Rathee <ab669522@gmail.com>
2022-08-22 14:08:56 +0530

    single folder generators, decode debug improvements, local colormap usage

--- src/decode.jl
+++ src/decode.jl
@@ -4,13 +4,13 @@ using FixedPointNumbers
 using FileIO
 
 """
-    gif_decode(filepath::AbstractString; info = false)
+gif_decode(filepath::AbstractString; use_localpalette=false)
 
 Decode the GIF image as colorant matrix. The source data needs to be a filename.
 
 #### Arguments
 - `filepath::AbstractString` : Path to the gif file
-- `info::Bool` : Describes presence of local color map and description of a image
+- `use_localpalette::Bool=true` : While decoding, using this argument use of local color map or global map can be specified
 
 #### Examples
 ```jldoctest
@@ -22,7 +22,7 @@ julia> path = "test/data/fire.gif"
 julia> img = gif_decode(path)
 60×30×33 Array{RGB{N0f8},3} with eltype RGB{N0f8}
 """
-function gif_decode(filepath::AbstractString; info = false)
+function gif_decode(filepath::AbstractString; use_localpalette=false)
     error = Cint(0)
     gif = LibGif.DGifOpenFileName(filepath, Ref(error))
 
@@ -40,7 +40,7 @@ function gif_decode(filepath::AbstractString; info = false)
         components = unsafe_wrap(Array, loaded_gif.SavedImages, loaded_gif.ImageCount)
 
         # Gif's are palette based and can have upto 256 colors in a image
-        colormap = unsafe_load(loaded_gif.SColorMap)
+        colormap = unsafe_load(loaded_gif.SColorMap) # global colormap
         palette = unsafe_wrap(Array, colormap.Colors, colormap.ColorCount)
 
         final = zeros(RGB{N0f8}, loaded_gif.SHeight, loaded_gif.SWidth, loaded_gif.ImageCount)
@@ -49,12 +49,16 @@ function gif_decode(filepath::AbstractString; info = false)
         for i = 1:img_count
             img = unsafe_wrap(Array, components[i].RasterBits, loaded_gif.SWidth * loaded_gif.SHeight)
             desc = components[i].ImageDesc
-            if info == true
-                print("Image $i at [$(desc.Left), $(desc.Top)] and size [$(desc.Width), $(desc.Height)]")
-                (desc.ColorMap !=C_NULL) ? print(" and has a local colormap.\n") : print(" and doesn't have a local colormap.\n")
-            end
-             # add support for working with local colormaps
             
+            @debug "Image $i at [$(desc.Left), $(desc.Top)] and size [$(desc.Width), $(desc.Height)]" * ((desc.ColorMap !=C_NULL) ? " and has a local colormap.\n" : " and doesn't have a local colormap.\n")
+            
+            # support for using local colormaps
+            if desc.ColorMap !=C_NULL && use_localpalette == true
+                localcolormap = unsafe_load(desc.ColorMap)
+                @debug "Image $i : using Local ColorMap with $(localcolormap.ColorCount) colors."
+                palette = unsafe_wrap(Array, localcolormap.Colors, localcolormap.ColorCount)
+            end
+
             colortypes = map(x -> palette[x+1], img)
             res = map(x -> RGB{N0f8}(x.Red / 255, x.Green / 255, x.Blue / 255), colortypes)
             res = reshape(res, Int(loaded_gif.SWidth), Int(loaded_gif.SHeight))
